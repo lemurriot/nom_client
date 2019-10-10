6d80da78435bc7c33b185b35394afaba0ffb405e
Kevin Boyle <papershack@gmail.com>
2019-10-09 23:00:56 -0700

    set up preliminary structure to render to screen

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { Switch, Route } from 'react-router-dom'
+import Main from './Main/Main'
+
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      error: null,
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route 
+            exact 
+            path="/" 
+            component={Main}
+          />
+        </Switch>
+      </div>
+    )
+  }
+}
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+
+export default function Header() {
+    return (
+        <nav className="header">
+            Header
+        </nav>
+    )
+}
