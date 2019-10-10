e1f67937d91a62df8b9c532908bb5325216245a0
Kevin Boyle <papershack@gmail.com>
2019-10-10 14:18:41 -0700

    wrangle css in header, add normalize import statment to index.js for css reset

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -10,14 +10,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function Header() {
     return (
         <header className="header">
-            <div className="brand">
-                <span className="logo"><FontAwesomeIcon icon="hamburger" color="#bc47ca"/></span>
-                <h1>NomsPDX</h1>
+            <div>
+            <Link to="/" style={{ textDecoration: 'none', height: '0'}}>
+                <div className="brand">
+                    <span className="logo"><FontAwesomeIcon icon="hamburger" color="#bc47ca"/></span>
+                    <h1>NomsPDX</h1>
+                </div>
+            </Link>
             </div>
             {/* <Link to="/"><FontAwesomeIcon icon="home" size="lg"/></Link> */}
             <nav>
-                <Link to="/">Login</Link>
-                <Link to="/">Register</Link>
+                <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Login</Link>
+                <Link to="/" style={{ textDecoration: 'none', color: 'white' }}>Register</Link>
             </nav>
         </header>
     )
