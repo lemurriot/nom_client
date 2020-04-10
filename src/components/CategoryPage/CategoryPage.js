520ce8690ece6bf894081fcb967951a20944dc3b
Kevin Boyle <boyle.kevin.michael@gmail.com>
2020-04-10 13:08:22 -0700

    add functional search bar to category page

--- src/components/CategoryPage/CategoryPage.js
+++ src/components/CategoryPage/CategoryPage.js
@@ -1,17 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import { Button, InputBase } from '@material-ui/core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useStyles } from '../../hooks/useStyles';
 import NomsContext from '../../NomsContext';
-import NominatedRestaurantPreview from '../NominatedRestaurantPreview/NominatedRestaurantPreview';
+import RestaurantPreview from '../RestaurantPreview/RestaurantPreview';
 import SelectMenu from '../SelectMenu/SelectMenu';
 import { sortRestaurants } from '../../utils';
 import { sortConstants } from '../../constants/sortConstants';
 
 const CategoryPage = () => {
   const { food_category: category } = useParams();
+  const classes = useStyles();
   const { goBack } = useHistory();
   const { nominatedRestaurants, voteTallies } = useContext(NomsContext);
   const [sortRestaurantsBy, setSortRestaurantsBy] = useState('All Time');
+  const [searchString, setSearchString] = useState('');
+  const re = new RegExp(searchString, 'i');
 
   const categoryRestaurants = nominatedRestaurants.filter(
     ({ food_category }) => food_category === category
@@ -27,29 +32,58 @@ const CategoryPage = () => {
     sortRestaurants(categoryRestaurants, voteTallies, sortRestaurantsBy);
   }, [categoryRestaurants, voteTallies, sortRestaurantsBy]);
 
-  const CategoryRestaurantList = sortedCategoryRestaurants.map((restaurant) => (
-    <NominatedRestaurantPreview
-      key={restaurant.id}
-      id={restaurant.id}
-      voteCount={voteTallies[restaurant.id]}
-      category={restaurant.food_category}
-      name={restaurant.name}
-      subtitle={restaurant.subtitle}
-    />
-  ));
+  let currentRestaurantSearchFilter;
+  if (!searchString) {
+    currentRestaurantSearchFilter = sortedCategoryRestaurants;
+  } else {
+    currentRestaurantSearchFilter = sortedCategoryRestaurants.filter(
+      (restaurant) => restaurant.name.match(re)
+    );
+  }
+
+  const CategoryRestaurantList = currentRestaurantSearchFilter.map(
+    (restaurant) => (
+      <RestaurantPreview
+        key={restaurant.id}
+        id={restaurant.id}
+        voteCount={voteTallies[restaurant.id]}
+        category={restaurant.food_category}
+        name={restaurant.name}
+        subtitle={restaurant.subtitle}
+      />
+    )
+  );
   return (
     <main className="page">
-      <Button onClick={goBack}>Go back</Button>
-      <h3>Nominated for Best {category}</h3>
-      <SelectMenu
-        menuOptions={sortConstants}
-        value={sortRestaurantsBy}
-        setSortBy={setSortRestaurantsBy}
-        helperText="Sort By"
-      />
-      <div className="restaurant-review-preview-box">
-        {CategoryRestaurantList}
-      </div>
+      <Button onClick={goBack} variant="contained" className="go-back-btn">
+        Go back
+      </Button>
+      <section className="content-container">
+        <h3>Nominated for Best {category}</h3>
+        <SelectMenu
+          menuOptions={sortConstants}
+          value={sortRestaurantsBy}
+          setSortBy={setSortRestaurantsBy}
+          helperText="Sort By"
+        />
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <FontAwesomeIcon icon="search" color="grey" />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+            onChange={(e) => setSearchString(e.target.value)}
+          />
+        </div>
+        <div className="restaurant-review-preview-box">
+          {CategoryRestaurantList}
+        </div>
+      </section>
     </main>
   );
 };
